3c3aad19215a2f20823b69811fcdb43386404f6c
Tomas Iñiguez <tomas.iniguez.gonzalez@gmail.com>
2020-05-24 16:37:00 -0400

    La comunicación con la db

--- api/functions/src/index.ts
+++ api/functions/src/index.ts
@@ -1,4 +1,15 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fir-75d5c.firebaseio.com"
+});
+
+const db = admin.firestore();
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +17,5 @@ import * as functions from 'firebase-functions';
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.json("Hello from Firebase!");
 });
+
+
